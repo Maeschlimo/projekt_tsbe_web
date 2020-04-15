a15287f4f93cb2bc6d3bdd847d20aca9fdc46628
Maeschlimo <tinellauener@gmail.com>
2020-04-15 23:08:51 +0200

    Init porject, login erstellt Co-authored-by: nst051211@stud.gibb.ch <nst051211@stud.gibb.ch>

---
+++ benutzeranzeige/src/router/index.js
@@ -0,0 +1,49 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+  const routes = [
+  {
+    path: '/',
+    redirect: {
+      name: "Login"
+    },
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home,
+
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
+  },
+  {
+      path: "/secure",
+      name: "secure",
+      component: () => import(/* webpackChunkName: "login" */ '../views/Secure.vue')
+  },
+  {
+  path: '/register',
+  name: 'Register',
+  component: () => import(/* webpackChunkName: "about" */ '../views/Register.vue')
+}
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ benutzeranzeige/src/views/Register.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h1>Bitte registriere dich:</h1>
+    <input type="vorname"
+    name="vorname"
+    v-model="vorname"
+    placeholder="Vorname"/>
+    <br>
+    <input type="name"
+    name="name"
+    v-model="email"
+    placeholder="Name"/>
+    <br>
+    <input type="email"
+    name="email"
+    v-model="email"
+    placeholder="E-Mail"/>
+    <br>
+    <input type="password"
+    name="password"
+    v-model="password"
+    placeholder="Passwort"/>
+    <br>
+    <button @click="register"> Register </button>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Register',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    register () {
+        //vorname: this.vorname
+        confirm (`Hallo Du hast dich erfolgreich registriert!`);
+    }
+  }
+}
+
+</script>
