17a922b45d788d0a9081e1b16f0626f29e214531
Nicola Staub <nicola_staub@bluewin.ch>
2020-04-16 12:33:35 +0200

    Registration überarbeitet

--- benutzeranzeige/src/views/Register.vue
+++ benutzeranzeige/src/views/Register.vue
@@ -1,30 +1,32 @@
 <template>
   <div>
     <h1>Bitte registriere dich:</h1>
-    <input type="vorname"
-    name="vorname"
-    v-model="vorname"
-    placeholder="Vorname"/>
-    <br>
-    <input type="name"
-    name="name"
-    v-model="name"
-    placeholder="Name"/>
-    <br>
-    <input type="email"
-    name="email"
-    v-model="email"
-    placeholder="E-Mail"/>
-    <br>
-    <input type="password"
-    name="password"
-    v-model="password"
-    @keyup.enter="register()"
-    placeholder="Passwort"/>
-    <br>
-    <button @click="register"> Register </button>
+    <form v-on:keyup.enter="validate">
+      <input type="vorname"
+      name="vorname"
+      v-model="vorname"
+      placeholder="Vorname"/>
+      <br>
+      <input type="name"
+      name="name"
+      v-model="name"
+      placeholder="Name"/>
+      <br>
+      <input type="email"
+      name="email"
+      v-model="email"
+      placeholder="E-Mail"/>
+      <br>
+      <input type="password"
+      name="password"
+      v-model="password"
+      placeholder="Passwort"/>
+      <br>
+      <button @click="validate"> Register </button>
+    </form>
   </div>
 </template>
+
 
 <script>
 
@@ -32,6 +34,8 @@ export default {
   name: 'Register',
   data () {
     return {
+      credentials: [],
+      credlabels: ['Vorname','Name','E-Mail','Passwort'],
       vorname:'',
       name:'',
       email: '',
@@ -39,19 +43,30 @@ export default {
     }
   },
   methods: {
-    register () { 
+    register () {
       if (confirm (`Hallo ${this.vorname} Du hast dich erfolgreich registriert!`)){
         this.$router.replace({ name: "Login" }); 
-        localStorage.setItem('vorname', this.vorname) 
-        localStorage.setItem('name', this.name) 
-        localStorage.setItem('email', this.email) 
-        localStorage.setItem('password', this.password)       
+        localStorage.setItem('credentials', this.credentials)    
       }
-    }
+    },
+    validate (){
+      this.credentials.push(this.vorname)
+      this.credentials.push(this.name)
+      this.credentials.push(this.email)
+      this.credentials.push(this.password)
+      let registration_valid = false;
+
+      for (let index = 0; index < 4; index++) {
+        if (this.credentials[index].length<1){
+          alert(`${this.credlabels[index]} ist ein Pflichtfeld`)
+        }  
+      }
+      
+      if (registration_valid===true){
+        this.register ()
+      }
   }
 }
-
-
+}
 </script>
 
-export.module = 
